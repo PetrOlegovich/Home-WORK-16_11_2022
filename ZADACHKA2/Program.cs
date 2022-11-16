0e91553e008b1632ed7f22e74b8e1d85fd01b47f
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-11-16 22:37:54 +0300

    найти третью цифру числа

---
+++ ZADACHKA2/Program.cs
@@ -0,0 +1,18 @@
+﻿// написать программу, которая выводит третью цифру заданного числа, или сообщает что третьей цифры нет.
+// Например : 
+// 645    -> 5
+// 78     -> третьей цифры нет
+// 32679  -> 6
+
+Console.WriteLine("Введите число : ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string str = num.ToString();
+
+if (num > 100)
+{
+    Console.WriteLine($"Третья цифра : {str[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
